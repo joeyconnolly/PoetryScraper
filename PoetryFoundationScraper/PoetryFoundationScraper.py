0a2f6d4f7a8ee1b08eeb416ec7d94156c2aac6fa
essvee <svince04@mail.bbk.ac.uk>
2017-08-07 08:03:48 +0100

    Starts adding index-scraping, need to fix recursion errors

--- PoetryFoundationScraper/PoetryFoundationScraper.py
+++ PoetryFoundationScraper/PoetryFoundationScraper.py
@@ -1,6 +1,31 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, SoupStrainer
+import httplib2
 import re
 import urllib.request
+
+i = 1
+index = set([])
+
+
+#  Scan over each page of poet index and store the urls in a dictionary for later
+def get_poets():
+    global i
+    index_page = "https://www.poetryfoundation.org/poets/browse#page=" + str(i) + "&sort_by=last_name&preview=0"
+    # Try to open each page in turn, if not possible, we've run out of pages
+    try:
+        build_index(index_page)
+        i += 1
+        get_poets()
+    except urllib.error.HTTPError as e:
+        print(e)
+        print("reached end of index")
+
+
+def build_index(index_url):
+    soup = BeautifulSoup(index_url, "html.parser")
+    for link in soup.find_all('a', href=re.compile('.*/poets/.*')):
+        print("Adding " + str(link) + "...")
+        index.add(link)
 
 
 def get_poems(page):
@@ -26,7 +51,6 @@ def get_poems(page):
             for div in annotations:
                 div.decompose()
 
-            # Todo - This is picking up non-poem elements, is there a better indicator?
             poem_content = poem_soup.find_all('div', {'style': "text-indent: -1em; padding-left: 1em;"})
             title_out = poem_title
 
@@ -40,12 +64,15 @@ def get_poems(page):
 
                 print("\n******\n", file=output)
 
-poet = input('Enter a poet: ').lower()
-poet = re.sub('[^a-z]+', '-', poet)
+# poet = input('Enter a poet: ').lower()
+# poet = re.sub('[^a-z]+', '-', poet)
+poet = "Seamus Heaney"
 url = "http://www.poetryfoundation.org/bio/" + poet + "#about"
 
-try:
-    page_in = urllib.request.urlopen(url)
-    get_poems(page_in)
-except urllib.error.HTTPError:
-    print("Nothing found for that poet")
+get_poets()
+
+# try:
+#     page_in = urllib.request.urlopen(url)
+#     get_poems(page_in)
+# except urllib.error.HTTPError:
+#     print("Nothing found for that poet")
